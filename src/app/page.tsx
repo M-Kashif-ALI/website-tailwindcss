eb913a5946f0f56f12218e93e56ed20e87fa0580
M-Kashif-ALI <158289645+M-Kashif-ALI@users.noreply.github.com>
2024-11-09 15:31:57 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Services from './components/services';
 export default function Home() {
   return (
     <div>
-      <div className="h-[700px] relative">
+      <div className="h-[700px] relative"> 
       <video src={bgVideo} autoPlay muted loop className="fixed right-0 top-0 h-[700px] w-full object-cover z-[-1]"/>
       <NavBar/>
       <Hero/>
